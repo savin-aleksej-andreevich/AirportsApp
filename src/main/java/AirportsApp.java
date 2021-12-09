1fb8c31c1396ea3398f1cb078cdd8adb621bd32b
Alexey Savin <therealsava@icloud.com>
2021-12-10 02:02:32 +0300

    Scripted auto-commit on change (2021-12-10 02:02:32) by gitwatch.sh

--- src/main/java/AirportsApp.java
+++ src/main/java/AirportsApp.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class AirportsApp {
+public class AirportsApp {
+
 }
--- src/main/java/AirportsMapper.java
+++ src/main/java/AirportsMapper.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class AirportsMapper {
+public class AirportsMapper {
 }
