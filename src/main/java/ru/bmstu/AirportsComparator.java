1115057843c071fecb3893eebc89f5f8a0cbde50
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:21:58 +0300

    Scripted auto-commit on change (2021-12-10 22:21:58) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsComparator.java
+++ src/main/java/ru/bmstu/AirportsComparator.java
@@ -1,9 +1,17 @@
 package ru.bmstu;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportsComparator extends WritableComparator {
     protected AirportsComparator () {
         super(AirportsWritableComparable.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable w1, WritableComparable w2) {
+        AirportsWritableComparable airport1 = (AirportsWritableComparable)w1;
+        AirportsWritableComparable airport2 = (AirportsWritableComparable)w2;
+        return Integer.compare(airport1.getAirportId(), airport2.getAirportId());
     }
 }
