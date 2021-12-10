d1f3138c8301e6d8bf88fe54129c5966e01fb657
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:18:39 +0300

    Scripted auto-commit on change (2021-12-10 22:18:39) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsPartitioner.java
+++ src/main/java/ru/bmstu/AirportsPartitioner.java
@@ -1,4 +1,12 @@
 package ru.bmstu;
 
-public class AirportsPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportsPartitioner extends Partitioner<AirportsWritableComparable, Text> {
+
+    @Override
+    public int getPartition(AirportsWritableComparable key, Text text, int numReduceTasks) {
+        return key.getAirportId() % numReduceTasks;
+    }
 }
