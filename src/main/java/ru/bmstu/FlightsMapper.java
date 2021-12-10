8f6badc5e1ac6c1ba0a84d6f02376412fa5be2fa
Alexey Savin <therealsava@icloud.com>
2021-12-10 20:50:14 +0300

    Scripted auto-commit on change (2021-12-10 20:50:14) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightsMapper.java
+++ src/main/java/ru/bmstu/FlightsMapper.java
@@ -4,6 +4,14 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightsMapper extends Mapper<LongWritable, Text, AirportsWritableComparable, Text> {
+import java.io.IOException;
 
+public class FlightsMapper extends Mapper<LongWritable, Text, AirportsWritableComparable, Text> {
+    public static int FLIGHT_INDICATOR = 1;
+    public static String SEPARATOR = ",";
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+    }
 }
