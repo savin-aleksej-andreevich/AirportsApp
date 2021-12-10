e1de869c69ba1a8ddfa13d6204bb7638afdb297b
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:29:21 +0300

    Scripted auto-commit on change (2021-12-10 22:29:21) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsReducer.java
+++ src/main/java/ru/bmstu/AirportsReducer.java
@@ -13,5 +13,15 @@ public class AirportsReducer extends Reducer <AirportsWritableComparable, Text, 
         float min = -1.0f, max = 0.0f, sum = 0.0f;
         Iterator<Text> valuesIterator = values.iterator();
         String airport = valuesIterator.next().toString();
+        while (valuesIterator.hasNext()) {
+            float delay =  Float.parseFloat(valuesIterator.next().toString());
+            if (delay > max) max = delay;
+            if (min == -1 || delay < min) min = delay;
+            sum = sum + delay;
+            count++;
+        }
+        if (count > 0) {
+            context.write(new Text("Airport: " + airport), new Text("min = " + min + ", max + " + max + ", sum = " + sum));
+        }
     }
 }
