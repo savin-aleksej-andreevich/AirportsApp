30b01b4ed0f0ff7413125ec9db9566c305d8c8ef
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:26:07 +0300

    Scripted auto-commit on change (2021-12-10 22:26:07) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsReducer.java
+++ src/main/java/ru/bmstu/AirportsReducer.java
@@ -4,10 +4,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportsReducer extends Reducer <AirportsWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(AirportsWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        
+        int count = 0;
+        float min = -1.0f, max = 0.0f, sum = 0.0f;
+        Iterator<Text> valuesIterator = values.iterator();
+        String airport = valuesIterator.next().toString();
     }
 }
