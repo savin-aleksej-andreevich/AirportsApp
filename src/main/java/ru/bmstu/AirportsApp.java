9b06d10163d29d4662a9b7db1c9cb99bbe4dc785
Alexey Savin <therealsava@icloud.com>
2021-12-10 19:55:28 +0300

    Scripted auto-commit on change (2021-12-10 19:55:28) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsApp.java
+++ src/main/java/ru/bmstu/AirportsApp.java
@@ -1,7 +1,29 @@
 package ru.bmstu;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class AirportsApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+        //hadoop jar target/WordCountApp-1.0-SNAPSHOT.jar WordCountApp warandpeace1.txt output
+        Job job = Job.getInstance();
+        job.setJarByClass(WordCountApp.class);
+        job.setJobName("Word count");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(WordMapper.class);
+        job.setReducerClass(WordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
