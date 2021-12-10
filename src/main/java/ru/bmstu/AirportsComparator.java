cbb26055bf8199892fe16fad3ccb8de4164e578b
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:23:58 +0300

    Scripted auto-commit on change (2021-12-10 22:23:58) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsComparator.java
+++ src/main/java/ru/bmstu/AirportsComparator.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportsComparator extends WritableComparator {
+
     protected AirportsComparator () {
         super(AirportsWritableComparable.class, true);
     }
--- src/main/java/ru/bmstu/AirportsReducer.java
+++ src/main/java/ru/bmstu/AirportsReducer.java
@@ -1,4 +1,13 @@
 package ru.bmstu;
 
-public class AirportsReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class AirportsReducer extends Reducer <AirportsWritableComparable, Text, Text, Text> {
+    @Override
+    protected void reduce(AirportsWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+    }
 }
