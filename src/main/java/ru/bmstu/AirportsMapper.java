52a1f54b27d93c29ea056801b5b5198b4e7a6d64
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:09:37 +0300

    Scripted auto-commit on change (2021-12-10 22:09:37) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsMapper.java
+++ src/main/java/ru/bmstu/AirportsMapper.java
@@ -1,4 +1,15 @@
 package ru.bmstu;
 
-public class AirportsMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+
+public class AirportsMapper extends Mapper<LongWritable, Text, AirportsWritableComparable, Text> {
+    private static int AIRPORD_INDICATOR = 0, INDEX_ID = 0, INDEX_NAME = 1;
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportsWritableComparable, Text>.Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
