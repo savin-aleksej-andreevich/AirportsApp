56db6f2e4206aaa3d2e65d2028565ad0b71aeca1
Alexey Savin <therealsava@icloud.com>
2021-12-10 22:16:28 +0300

    Scripted auto-commit on change (2021-12-10 22:16:28) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsMapper.java
+++ src/main/java/ru/bmstu/AirportsMapper.java
@@ -12,7 +12,7 @@ public class AirportsMapper extends Mapper<LongWritable, Text, AirportsWritableC
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, AirportsWritableComparable, Text>.Context context) throws IOException, InterruptedException {
         String[] params = value.toString().split("\",\"");
         if (key.get() > 0) {
-            int airportId = Integer.parseInt(params[INDEX_ID].split("\"")[0]);
+            int airportId = Integer.parseInt(params[INDEX_ID].split("\"")[1]);
             String airportName = params[INDEX_NAME].split("\"")[0];
             context.write(new AirportsWritableComparable(airportId, AIRPORD_INDICATOR), new Text(airportName));
         }
