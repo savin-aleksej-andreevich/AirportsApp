50054b485c7edd0a4f1ce29990f55910189d4317
Alexey Savin <therealsava@icloud.com>
2021-12-10 21:11:53 +0300

    Scripted auto-commit on change (2021-12-10 21:11:53) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightsMapper.java
+++ src/main/java/ru/bmstu/FlightsMapper.java
@@ -7,11 +7,20 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, AirportsWritableComparable, Text> {
-    public static int FLIGHT_INDICATOR = 1;
-    public static String SEPARATOR = ",";
+    private static int FLIGHT_INDICATOR = 1;
+    private static int INDEX_AIRPORT = 14;
+    private static int INDEX_DELAY = 18;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+        String[] params = value.toString().split(",");
+        if (key.get() > 0) {
+            String delayString = params[INDEX_DELAY];
+            float delay = delayString.isEmpty()? 0.0f : Float.parseFloat(delayString);
+            if (delay > 0.0) {
+                int airportId = Integer.parseInt(params[INDEX_AIRPORT]);
+                context.write(new AirportsWritableComparable(airportId, FLIGHT_INDICATOR), new Text(delayString));
+            }
+        }
     }
 }
